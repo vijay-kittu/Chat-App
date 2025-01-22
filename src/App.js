e50fb56f727ab0171eee5bb7e9289543a6b691ee
vijay-kittu <vijaykittu16@gmail.com>
2025-01-23 04:34:50 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './styles.css';
-import Home from './pages/Home';
-import Chat from './pages/Chat';
-import PrivateChat from './pages/PrivateChat';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import Home from './components/Home';
+import Chat from './components/Chat';
+import PrivateChat from './components/PrivateChat';
+import Register from './components/Register';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
-import { AuthProvider } from './context/AuthContext';
+import './App.css'
+//import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
+    //<AuthProvider>
       <Router>
         <Navbar />
         <Routes>
@@ -23,9 +23,10 @@ function App() {
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
-    </AuthProvider>
+    //</AuthProvider>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
+  <div className='home'>
+    <div className='head'>
+      <h1>HyThere!</h1>
+      <div>
+        <div>Profile</div>
+        <div>Theme</div>
+        <div>Logout</div>
+      </div>
+    </div>
     <h1>Welcome to Chat App</h1>
     <p>Connect with people, send friend requests, and chat privately.</p>
   </div>
