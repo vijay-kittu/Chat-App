d3caef760e20a80f0d1b70c27c0a7d3d502120a3
vijay-kittu <vijaykittu16@gmail.com>
2025-02-07 23:59:37 +0530

    made changes to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ const Contact = () => {
         <div className='contact'>
             <div>Connect with us on:</div>
             <a href="https://www.instagram.com/vijay.kittu/">Instagram</a>
+            <a href="https://www.linkedin.com/in/vijay-krishna-4b7295253/">Linkedin</a>
         </div>
     );
 }
