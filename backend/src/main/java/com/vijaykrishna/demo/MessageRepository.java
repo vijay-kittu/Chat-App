466846c84d99066e53311584f1af540638803680
vijay-kittu <vijaykittu16@gmail.com>
2025-02-08 16:14:56 +0530

    made changes in Register.js and some backend restructures

--- backend/src/main/java/com/vijaykrishna/demo/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vijaykrishna.demo;
-
-import com.vijaykrishna.demo.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
