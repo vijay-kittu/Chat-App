27211e6b830a2f363c5cc3d2d608d058ef2f92dc
vijay-kittu <vijaykittu16@gmail.com>
2025-02-23 23:56:52 +0530

    changes in home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,34 @@
-import React, {useContext} from 'react';
-import Chat from './Chat';
-import { Link } from 'react-router-dom';
-import PrivateChat from './PrivateChat';
-import Contact from './Contact';
-import Navbar from './Navbar';
-import { AuthContext } from './AuthContext';
+import React, { useContext } from "react";
+import Chat from "./Chat";
+import { Link } from "react-router-dom";
+import PrivateChat from "./PrivateChat";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import { AuthContext } from "./AuthContext";
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
 
-  const {user} = useContext(AuthContext);
-
-  if(!user){
-    return(
+  if (!user) {
+    return (
       <div>
         <h2>Please</h2>
         <Link to="/login">login</Link>
         <h2>to continue.</h2>
       </div>
-      
-    ) ;
+    );
   }
 
-  return(
-    <div className='home'>
-      <div className='head'>
-        <h2>HYTHERE!</h2>
+  return (
+    <div className="home">
+      <div className="head">
+        <h2>Hythere!</h2>
       </div>
       <h3>Welcome to Chat App, {user.userName}</h3>
       <Navbar />
       <div className="homepage_distribution">
         <Chat />
         <PrivateChat />
-        
       </div>
       <Contact />
     </div>
