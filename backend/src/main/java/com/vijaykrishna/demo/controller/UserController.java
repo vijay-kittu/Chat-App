6d1e7054a3768ed2e8a6024952c1ee0969710f52
vijay-kittu <vijaykittu16@gmail.com>
2025-03-27 00:39:20 +0530

    changes in backend

--- backend/src/main/java/com/vijaykrishna/demo/controller/UserController.java
+++ backend/src/main/java/com/vijaykrishna/demo/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins={"http://localhost:3000", "https://portfolio-vijaykrishna.vercel.app/"})
 public class UserController {
 
     @Autowired
